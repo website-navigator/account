e61c62765eca019a4ae33b6876d6490cc7564bf7
Bai Jie <baijie1991@gmail.com>
2016-08-25 07:15:47 +0800

    improve SubjectService interface

--- code/api/src/main/java/gq/optimalorange/account/SubjectService.java
+++ code/api/src/main/java/gq/optimalorange/account/SubjectService.java
@@ -1,20 +1,21 @@
 package gq.optimalorange.account;
 
+import javax.annotation.Nonnull;
+
 import rx.Single;
 
 public interface SubjectService extends Service {
 
-  Single<Result<Void, CreateFailureCause>> create(Identifier identifier);
+  /**
+   * @return the ID of new created Subject
+   */
+  Single<Result<Identifier, Void>> create();
 
-  enum  CreateFailureCause {
-    ALREADY_EXIST
-  }
+  Single<Result<Void, Void>> exist(@Nonnull Identifier identifier);
 
-  Single<Result<Void, Void>> exist(Identifier identifier);
+  Single<Result<Identifier, GetIdentifierFailureCause>> getId(@Nonnull Identifier identifier);
 
-  Single<Result<Identifier, GetIdentifierFailureCause>> getId(Identifier identifier);
-
-  Single<Result<Identifier, GetIdentifierFailureCause>> getUserName(Identifier identifier);
+  Single<Result<Identifier, GetIdentifierFailureCause>> getUserName(@Nonnull Identifier identifier);
 
   enum GetIdentifierFailureCause {
     NOT_EXIST
