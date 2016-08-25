10f45475c9fa218f9df6e9ad6d5d832a0d6f8204
Bai Jie <baijie1991@gmail.com>
2016-08-25 15:32:21 +0800

    change Certificate to concrete class

--- code/api/src/main/java/gq/optimalorange/account/Certificate.java
+++ code/api/src/main/java/gq/optimalorange/account/Certificate.java
@@ -4,18 +4,31 @@ import javax.annotation.Nonnull;
 
 import okio.ByteString;
 
-public abstract class Certificate {
+public class Certificate {
 
   public static final String TYPE_PASSWORD = "password";
 
+  private final String type;
+
   private final ByteString value;
 
-  protected Certificate(@Nonnull ByteString value) {
+  public static Certificate password(@Nonnull String password) {
+    return create(TYPE_PASSWORD, ByteString.encodeUtf8(password).sha256());
+  }
+
+  private static Certificate create(@Nonnull String type, @Nonnull ByteString value) {
+    return new Certificate(type, value);
+  }
+
+  protected Certificate(@Nonnull String type, @Nonnull ByteString value) {
+    this.type = type;
     this.value = value;
   }
 
   @Nonnull
-  public abstract String getType();
+  public String getType() {
+    return type;
+  }
 
   @Nonnull
   public ByteString getValue() {
