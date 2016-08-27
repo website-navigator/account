4e79fdcd0087812f7ec56f431afdc69df3cb074e
Bai Jie <baijie1991@gmail.com>
2016-08-27 16:14:46 +0800

    add async authenticate samples

--- code/sample/src/main/java/gq/optimalorange/account/integration/Main.java
+++ code/sample/src/main/java/gq/optimalorange/account/integration/Main.java
@@ -1,9 +1,13 @@
 package gq.optimalorange.account.integration;
 
+import java.util.concurrent.CountDownLatch;
+
 import gq.optimalorange.account.AuthenticationService;
+import gq.optimalorange.account.Result;
 import gq.optimalorange.account.integration.inject.DaggerMainComponent;
 import gq.optimalorange.account.integration.inject.MainComponent;
 import gq.optimalorange.account.integration.utils.Debugger;
+import rx.schedulers.Schedulers;
 
 import static gq.optimalorange.account.Certificate.password;
 import static gq.optimalorange.account.Identifier.id;
@@ -36,20 +40,72 @@ public class Main implements Runnable {
   private void doTask() {
     mainComponent = DaggerMainComponent.create();
     println(mainComponent.getSubjectService());
-    final AuthenticationService auth = mainComponent.getAuthenticationService();
-    println(auth);
-    auth.authenticate(id("1"), password("test")).subscribe(result -> {
-      if (result.succeeded()) {
-        println("authenticate succeeded!");
-      } else {
-        println("authenticate failed");
-        println("cause: " + result.cause());
+    println(mainComponent.getAuthenticationService());
+
+    System.out.println();
+    authenticateUsingMainThread();
+    System.out.println();
+    authenticate();
+    System.out.println();
+    authenticateBlocking();
+  }
+
+  private void authenticateUsingMainThread() {
+    mainComponent.getAuthenticationService()
+        .authenticate(id("1"), password("test"))
+        .subscribe(result -> {
+          if (result.succeeded()) {
+            println("authenticate succeeded!");
+          } else {
+            println("authenticate failed");
+            println("cause: " + result.cause());
+          }
+        });
+  }
+
+  private void authenticate() {
+    CountDownLatch latch = new CountDownLatch(1);
+    mainComponent.getAuthenticationService().authenticate(id("1"), password("test"))
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.computation())
+        // start authenticate
+        .subscribe(result -> {
+          if (result.succeeded()) {
+            println("authenticate succeeded!");
+          } else {
+            println("authenticate failed");
+            println("cause: " + result.cause());
+          }
+          latch.countDown();
+        });
+    // wait authenticate finished
+    while (true) {
+      try {
+        latch.await();
+        break;
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
-    });
+    }
+  }
+
+  private void authenticateBlocking() {
+    final Result<Void, AuthenticationService.AuthenticateFailureCause> result =
+        mainComponent.getAuthenticationService().authenticate(id("1"), password("test"))
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.computation())
+            .toBlocking()
+            .value();
+    if (result.succeeded()) {
+      println("blocking authenticate succeeded!");
+    } else {
+      println("blocking authenticate failed");
+      println("cause: " + result.cause());
+    }
   }
 
   private static void println(Object x) {
-    System.out.println(x);
+    System.out.println("[" + Thread.currentThread() + "]" + x);
   }
 
 }
