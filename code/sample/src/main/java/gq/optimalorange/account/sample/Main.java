9a981d162d38148ed34537c0e3f47d2c3cd0eb31
Bai Jie <baijie1991@gmail.com>
2016-08-30 11:54:22 +0800

    update sample

--- code/sample/src/main/java/gq/optimalorange/account/sample/Main.java
+++ code/sample/src/main/java/gq/optimalorange/account/sample/Main.java
@@ -2,15 +2,19 @@ package gq.optimalorange.account.sample;
 
 import java.util.concurrent.CountDownLatch;
 
-import gq.optimalorange.account.AuthenticationService;
+import gq.optimalorange.account.AuthenticationService.AuthenticateFailureCause;
+import gq.optimalorange.account.AuthenticationService.ChangeCertificateFailureCause;
+import gq.optimalorange.account.Identifier;
 import gq.optimalorange.account.Result;
+import gq.optimalorange.account.SubjectService.CreateFailure;
+import gq.optimalorange.account.SubjectService.SetIdentifierFailure;
 import gq.optimalorange.account.sample.inject.DaggerMainComponent;
 import gq.optimalorange.account.sample.inject.MainComponent;
 import gq.optimalorange.account.sample.utils.Debugger;
-import rx.schedulers.Schedulers;
+import gq.optimalorange.account.subject.utils.Pair;
+import rx.Observable;
 
 import static gq.optimalorange.account.Certificate.password;
-import static gq.optimalorange.account.Identifier.id;
 
 public class Main implements Runnable {
 
@@ -43,41 +47,28 @@ public class Main implements Runnable {
     println(mainComponent.getAuthenticationService());
 
     System.out.println();
-    authenticateUsingMainThread();
+    System.out.println("#doAsynchronously");
+    doAsynchronously();
     System.out.println();
-    authenticate();
-    System.out.println();
-    authenticateBlocking();
+    System.out.println("#doBlockingly");
+    doBlockingly();
   }
 
-  private void authenticateUsingMainThread() {
-    mainComponent.getAuthenticationService()
-        .authenticate(id("1"), password("test"))
-        .subscribe(result -> {
-          if (result.succeeded()) {
-            println("authenticate succeeded!");
-          } else {
-            println("authenticate failed");
-            println("cause: " + result.cause());
-          }
-        });
-  }
-
-  private void authenticate() {
+  private void doAsynchronously() {
     CountDownLatch latch = new CountDownLatch(1);
-    mainComponent.getAuthenticationService().authenticate(id("1"), password("test"))
-        .subscribeOn(Schedulers.io())
-        .observeOn(Schedulers.computation())
-        // start authenticate
+    getUseCase()
+        // start
         .subscribe(result -> {
           if (result.succeeded()) {
-            println("authenticate succeeded!");
+            println("succeeded: " + result.result());
           } else {
-            println("authenticate failed");
-            println("cause: " + result.cause());
+            println("failed cause: " + result.cause());
           }
+        }, e -> {
+          println(String.format(
+              "error:[%s]%s", e.getClass().getSimpleName(), e.getLocalizedMessage()));
           latch.countDown();
-        });
+        }, latch::countDown);
     // wait authenticate finished
     while (true) {
       try {
@@ -89,19 +80,64 @@ public class Main implements Runnable {
     }
   }
 
-  private void authenticateBlocking() {
-    final Result<Void, AuthenticationService.AuthenticateFailureCause> result =
-        mainComponent.getAuthenticationService().authenticate(id("1"), password("test"))
-            .subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.computation())
-            .toBlocking()
-            .value();
-    if (result.succeeded()) {
-      println("blocking authenticate succeeded!");
-    } else {
-      println("blocking authenticate failed");
-      println("cause: " + result.cause());
+  private void doBlockingly() {
+    try {
+      getUseCase().toBlocking().toIterable().forEach(result -> {
+        if (result.succeeded()) {
+          println("succeeded: " + result.result());
+        } else {
+          println("failed cause: " + result.cause());
+        }
+      });
+    } catch (Exception e) {
+      println(String.format("error:[%s]%s", e.getClass().getSimpleName(), e.getLocalizedMessage()));
+      e.printStackTrace();
     }
+  }
+
+  /*
+  1. create account
+  2. set user name
+  3. change password
+  4. authenticate
+  * */
+  private Observable<Result<?, ? extends Enum<? extends Enum<?>>>> getUseCase() {
+    final double suffix = Math.random() * 1_000_000;
+    final String initPassword = "init test password " + suffix;
+    final String username = "test username " + suffix;
+    final String changedPassword = "changed test password" + suffix;
+
+    final Observable<Result<Identifier, CreateFailure>> signUp =
+        mainComponent.getSubjectService().create(password(initPassword)).toObservable().cache();
+    final Observable<Result<Void, SetIdentifierFailure>> setUsername = signUp
+        .filter(Result::succeeded)
+        .flatMap(r -> mainComponent.getSubjectService()
+            .setIdentifier(r.result(), Identifier.username(username))
+            .toObservable());
+    final Observable<Result<Void, ChangeCertificateFailureCause>> changePassword = signUp
+        .filter(Result::succeeded)
+        .flatMap(r -> mainComponent.getAuthenticationService()
+            .changeCertificate( //TODO remove oldPassword parameter
+                                r.result(), password(initPassword), password(initPassword),
+                                password(changedPassword))
+            .toObservable())
+        .cache();
+
+    final Observable<Result<Void, AuthenticateFailureCause>> authenticate =
+        changePassword
+            .map(r -> {
+              if (r.succeeded()) {
+                return changedPassword;
+              } else {
+                return initPassword;
+              }
+            })
+            .zipWith(signUp, Pair::new)
+            .flatMap(r -> mainComponent.getAuthenticationService()
+                .authenticate(r.b.result(), password(r.a))
+                .toObservable());
+
+    return Observable.merge(signUp, setUsername, changePassword, authenticate);
   }
 
   private static void println(Object x) {
